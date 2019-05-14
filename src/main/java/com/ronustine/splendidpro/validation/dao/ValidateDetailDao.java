78e7011992f2aea262ebc77150b36d75bd4f2bd8
ronustine <diaoxin>
2019-05-14 19:12:05 +0800

    1. 修改中文、中英文校验 2. 增加mybatisGenerator插件

--- src/main/java/com/ronustine/splendidpro/validation/dao/ValidateDetailDao.java
+++
@@ -1,9 +0,0 @@
-package com.ronustine.splendidpro.validation.dao;
-
-import com.ronustine.splendidpro.validation.entity.po.ValidateDetail;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface ValidateDetailDao extends Mapper<ValidateDetail> {
-
-
-}
